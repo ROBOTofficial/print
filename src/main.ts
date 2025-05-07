7d7bcb55d98a6a70fb376bc3e4b15ccca14cebf4
ROBOT <tkata12@outlook.jp>
2025-05-07 20:23:59 +0900

    add: create pull request

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,45 @@
 import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+import { generateBot, generatePRBody } from "./bot.js";
 
 export async function run() {
 	const outputFile = core.getInput("output-file");
 	const ghToken = core.getInput("github-token");
+	const branch = github.context.ref.replace("refs/heads/", "");
+	const printerBranch = `gh-printer/${branch}`;
 
 	if (!outputFile) {
-		core.error(Error("output-file is not set"));
+		core.setFailed(Error("output-file is not set"));
 	}
 	if (!ghToken) {
-		core.error(Error("github-token is not set"));
+		core.setFailed(Error("github-token is not set"));
+	}
+
+	const octokit = generateBot(ghToken);
+	const prs = await octokit.rest.pulls.list({
+		...github.context.repo,
+		state: "open",
+		head: `${github.context.repo.owner}:${printerBranch}`,
+		base: branch
+	});
+
+	if (!prs.data.length) {
+		await octokit.rest.pulls.create({
+			base: branch,
+			head: printerBranch,
+			title: "",
+			body: generatePRBody(),
+			...github.context.repo
+		});
+	} else {
+		const [pr] = prs.data;
+		await octokit.rest.pulls.update({
+			...github.context.repo,
+			pull_number: pr.number,
+			title: "",
+			body: generatePRBody(),
+			state: "open"
+		});
 	}
 }
