6d0356e6066a5e8997cd388549d00a223ea136dc
ROBOT <tkata12@outlook.jp>
2025-05-07 17:33:17 +0900

    add: github-token and outpu-file params

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-export async function run() {}
+import * as core from "@actions/core";
+
+export async function run() {
+	const outputFile = core.getInput("output-file");
+	const ghToken = core.getInput("github-token");
+
+	if (!outputFile) {
+		core.error(Error("output-file is not set"));
+	}
+	if (!ghToken) {
+		core.error(Error("github-token is not set"));
+	}
+}
